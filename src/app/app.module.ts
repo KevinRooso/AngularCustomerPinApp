77bb0e954e8aa430bdaeb9e46cd18404e8bfb6b2
Yash Rikhi <yashrikhi@gmail.com>
2024-03-24 19:28:02 +0530

    Creating a Pins listing component with a header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PinsListComponent } from './views/pins-list/pins-list.component';
+import { HeaderComponent } from './views/header/header.component';
+import { AddCustomerComponent } from './views/add-customer/add-customer.component';
+import { FormsModule } from '@angular/forms';
+import { NgxSelectModule } from 'ngx-select-ex';
+import { HttpClientModule } from '@angular/common/http';
+import { AddPinComponent } from './views/add-pin/add-pin.component';
+import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PinsListComponent,
+    HeaderComponent,
+    AddCustomerComponent,
+    AddPinComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgxSelectModule,
+    HttpClientModule,
+    FileUploadModule
   ],
   providers: [],
   bootstrap: [AppComponent]
